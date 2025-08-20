7bebb0b26e11597a662e152d490ea905449bcb26
Rohitkumar367 <rohitddroid@gmail.com>
2025-08-21 00:31:37 +0530

    doing logo user content

--- app/create/_components/HeadingDescription.jsx
+++ app/create/_components/HeadingDescription.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const HeadingDescription = ({title, description}) => {
     return (
-        <div>HeadingDescription</div>
+        <div>
+            <h2 className='font-bold text-3xl text-primary'>{title}</h2>
+            <p className='text-lg text-gray-500 mt-2'>{description}</p>
+        </div>
     )
 }
 
 export default HeadingDescription
+
